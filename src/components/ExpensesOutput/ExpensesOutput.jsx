670cf25ab10d6f3508aec898940362b4a62454fc
pnxu <fcaavv@gmail.com>
2023-08-12 23:54:48 -0400

    feat(ExpensesOutput): Implement ExpenseItem, ExpenseList, ExpensesOutput & ExpensesSummary components

---
+++ src/components/ExpensesOutput/ExpensesOutput.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
+
+import ExpensesSummary from './ExpensesSummary';
+import ExpensesList from './ExpensesList';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32,
+  },
+});
+
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
+    </View>
+  );
+}
+
+export default ExpensesOutput;
