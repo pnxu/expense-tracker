9855b7c4abc30c04e516063a98f0dd5c090bffbf
pnxu <fcaavv@gmail.com>
2023-08-12 23:50:32 -0400

    feat(screens): Implement AllExpenses, ManageExpense, RecentExpense files

---
+++ src/screens/AllExpenses.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses.context';
+
+function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expense found!"
+    />
+  );
+}
+
+export default AllExpenses;
---
+++ src/screens/ManageExpense.jsx
@@ -0,0 +1,94 @@
+import React, { useContext, useLayoutEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import IconButton from '../components/UI/IconButton';
+import Button from '../components/UI/Button';
+import { GlobalStyles } from '../constants/styles';
+import { ExpensesContext } from '../store/expenses.context';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
+
+function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: 'TEST!!!',
+        amount: 10.0,
+        date: new Date('2022-12-15'),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: 'Test',
+        amount: 9.99,
+        date: new Date('2022-06-12'),
+      });
+    }
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default ManageExpense;
