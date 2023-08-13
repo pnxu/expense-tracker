b8ab56f0720610b8a315edcb990c90ab114c9d45
pnxu <fcaavv@gmail.com>
2023-08-12 23:51:58 -0400

    feat(constants): Implement styles in constant file

---
+++ src/constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#1B1B1B', // Background color
+    primary100: '#2C2C2C', // Secondary background color
+    primary200: '#800080', // Primary magenta for buttons and accents
+    primary400: '#A040A0', // Slightly lighter magenta for hover states
+    primary500: '#FF6AC1', // Pinkish magenta for highlights
+    primary700: '#990099', // Darker magenta for active states
+    primary800: '#AA00AA', // Vibrant magenta for special accents
+    accent500: '#00BFFF', // Accent color for links and special elements
+    error50: '#870000', // Dark red for subtle error messages
+    error500: '#D00000', // Bright red for critical errors
+    gray500: '#606060', // Medium dark gray for secondary text
+    gray700: '#404040', // Dark gray for borders or tertiary text
+  },
+};
